648938a71464aa5261e7daab84820cf42af0e93f
Daniel Clark <danclark1820@gmail.com>
2020-02-22 09:27:02 -0500

    got scroll sort of working

--- assets/js/water_cooler.js
+++ assets/js/water_cooler.js
@@ -7,6 +7,7 @@ let WaterCooler = {
 
   listenForChats(channel) {
     let userName = window.userName
+    let chatWindow = document.getElementById('chat-window')
     document.getElementById('chat-form').addEventListener('submit', function(e){
       e.preventDefault()
       let userMsg = document.getElementById('user-msg').value
@@ -15,14 +16,15 @@ let WaterCooler = {
 
       // document.getElementById('user-name').value = ''
       document.getElementById('user-msg').value = ''
+      // Re get chat window element to find out its new scroll height
+      chatWindow.scrollTop = document.getElementById('chat-window').scrollHeight
     })
 
     channel.on('shout', payload => {
-      let chatBox = document.querySelector('#chat-box')
       let msgBlock = document.createElement('p')
 
       msgBlock.insertAdjacentHTML('beforeend', `${payload.name}: ${payload.body}`)
-      chatBox.appendChild(msgBlock)
+      chatWindow.appendChild(msgBlock)
     })
   }
 }
