8db47c41f5d550d0f4b57aa842db4942f94249e6
Daniel Clark <danclark1820@gmail.com>
2020-06-10 10:06:03 -0400

    working through making oembed requests work

--- lib/tree_chat_web/views/page_view.ex
+++ lib/tree_chat_web/views/page_view.ex
@@ -13,10 +13,18 @@ defmodule TreeChatWeb.PageView do
     |> Floki.find("a")
 
     # [ {"a", [{"href", "https://www.youtube.com/watch?v=4VcTigzWfeQ"}],["https://www.youtube.com/watch?v=4VcTigzWfeQ"]} ]
+    link_preview = case links do
+      [] -> ""
+      ast_links -> hd(ast_links)
+        |> url_from_ast_link
+        |> requestOembed
+        |> compose_preview
+    end
 
     body
     |> Earmark.as_html!
     |> raw
+    # |> link_preview
     # |> append_link_preview(links)
   end
 
@@ -32,19 +40,26 @@ defmodule TreeChatWeb.PageView do
   def requestOembed(full_url) do
     case HTTPoison.get("#{base_url(full_url)}/oembed?url=#{full_url}&format=json") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        require IEx; IEx.pry
         Poison.Parser.parse! body
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
+        "Not found :("
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+        ""
     end
   end
 
-  def compose_preview(ombed_json) do
-    #You left off here, not sure if request oembed works yet
+  def compose_preview(%{title: some_title}) do
+    require IEx; IEx.pry
+    some_title
+    #You left off here, not sure if request oembed works yet6
   end
 
-  def base_url(url)
+  def compose_preview(error) do
+    error
+  end
+
+  def base_url(url) do
     Regex.replace(~r/(?:\/\/)|(\/.*){1}/, url, "")
   end
 end
