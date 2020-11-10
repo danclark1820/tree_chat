f4babbab47d842daf515a7a9e6723c1fa54b3b56
Daniel Clark <danclark1820@gmail.com>
2020-11-10 15:48:10 -0500

    fix search

--- lib/tree_chat_web/controllers/search_controller.ex
+++ lib/tree_chat_web/controllers/search_controller.ex
@@ -3,8 +3,10 @@ defmodule TreeChatWeb.SearchController do
   alias TreeChat.{Chat, Search, Message, Repo}
 
   def index(conn, params) do
+    oauth_google_url = TreeChat.AuthGoogle.generate_oauth_url(conn)
     message_results = Message |> Search.Messages.run(params["search"]["query"]) |> Repo.all
     chat_results = Chat |> Search.Chats.run(params["search"]["query"]) |> Repo.all
-    render conn, "index.html", messages: message_results, chats: chat_results, current_chat: nil
+
+    render conn, "index.html", messages: message_results, chats: chat_results, current_chat: nil, oauth_google_url: oauth_google_url
   end
 end
